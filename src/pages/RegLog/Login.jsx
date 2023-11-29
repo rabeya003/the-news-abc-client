52670858971ef5021e0f272a4101b3ba9e9b9b02
rabeya003 <rabeya15-13483@diu.edu.bd>
2023-11-30 03:11:15 +0600

    update login form

---
+++ src/pages/RegLog/Login.jsx
@@ -0,0 +1,68 @@
+import { useContext } from "react";
+import { ContextProvider } from "../../Providers/Authprovider";
+import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const { login } = useContext(ContextProvider);
+  const [sucsess, setSuccess] = useState("");
+  const [error, setError] = useState("");
+  const handleLog = (e) => {
+    e.preventDefault();
+    setError("");
+    setSuccess("");
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(email, password);
+    login(email, password)
+      .then((result) => {
+        const user = result.user;
+        setSuccess("user succesfully registerd");
+        e.target.reset();
+      })
+      .catch((error) => {
+        setError(error.massage);
+      });
+  };
+  return (
+    <div>
+      <form onSubmit={handleLog}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            required
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            name="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button className="mb-2" variant="primary" type="submit">
+          Login
+        </Button>
+        <br></br>
+        <Link to="/reg" variant="primary">
+          New to this website go Register
+        </Link>
+      </form>
+      <p>{error}</p>
+      <p>{sucsess}</p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/RegLog/Register.jsx
@@ -0,0 +1,88 @@
+import { useContext } from "react";
+import { ContextProvider } from "../../Providers/Authprovider";
+import { Button, Form } from "react-bootstrap";
+import { useState } from "react";
+import { sendEmailVerification } from "firebase/auth";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const { creatUser } = useContext(ContextProvider);
+  const [sucsess, setSuccess] = useState("");
+  const [error, setError] = useState("");
+  const handleReg = (e) => {
+    setError("");
+    setSuccess("");
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const name = e.target.name.value;
+    console.log(email, password, name);
+    creatUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        setSuccess("user succesfully registerd");
+        e.target.reset();
+        emailVeri(user);
+      })
+      .catch((error) => {
+        setError(error.massage);
+      });
+  };
+  const emailVeri = (user) => {
+    sendEmailVerification(user)
+      .then(() => {
+        setSuccess("Check your mail");
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  };
+  return (
+    <div>
+      <Form onSubmit={handleReg}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            required
+            placeholder="Enter Your name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            required
+            placeholder="Enter email"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            required
+            name="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+        <br></br>
+        <Link to="/login" variant="primary">
+          New to this website go login
+        </Link>
+        <p>{error}</p>
+        <p>{sucsess}</p>
+      </Form>
+    </div>
+  );
+};
+
+export default Register;
